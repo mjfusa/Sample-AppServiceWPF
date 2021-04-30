940a261cf8693921c3e4636fdaba46a8e0a598ba
Mike Francis <mike.francis@live.com>
2021-04-30 14:27:02 -0700

    Add project files.

---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private AppServiceConnection inventoryService;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Add the connection.
+            if (this.inventoryService == null)
+            {
+                this.inventoryService = new AppServiceConnection();
+
+                // Here, we use the app service name defined in the app service 
+                // provider's Package.appxmanifest file in the <Extension> section.
+                this.inventoryService.AppServiceName = "com.microsoft.inventory";
+
+                // Use Windows.ApplicationModel.Package.Current.Id.FamilyName 
+                // within the app service provider to get this value.
+                this.inventoryService.PackageFamilyName = "79a69251-101f-43f2-acc8-ea0de2df3551_n3sawgb4qe5x4";
+
+                var status = await this.inventoryService.OpenAsync();
+
+                if (status != AppServiceConnectionStatus.Success)
+                {
+                    textBox.Text = "Failed to connect";
+                    this.inventoryService = null;
+                    return;
+                }
+            }
+
+            // Call the service.
+            int idx = int.Parse(textBox.Text);
+            var message = new ValueSet();
+            message.Add("Command", "Item");
+            message.Add("ID", idx);
+            AppServiceResponse response = await this.inventoryService.SendMessageAsync(message);
+            string result = "";
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data  that the service sent to us.
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result = response.Message["Result"] as string;
+                }
+            }
+
+            message.Clear();
+            message.Add("Command", "Price");
+            message.Add("ID", idx);
+            response = await this.inventoryService.SendMessageAsync(message);
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data that the service sent to us.
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result += " : Price = " + response.Message["Result"] as string;
+                }
+            }
+
+            textResult.Text = result;
+        }
+    }
+}
